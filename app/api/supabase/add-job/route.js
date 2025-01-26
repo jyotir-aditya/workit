9adc63d947a2630f0040819ac162312c3200ed64
Aditya <adityaroy2169@gmail.com>
2025-01-26 16:24:20 +0530

    bug fix

--- app/api/supabase/add-job/route.js
+++ app/api/supabase/add-job/route.js
@@ -8,7 +8,10 @@ export async function POST(req) {
   }
   const admin_id = session.user.id;
   const { title, description, salary } = await req.json();
-  console.log(title, description, salary);
+  console.log(title, description, salary, admin_id);
+  if(!title || !description || !salary || !admin_id) {
+    return new Response("Error", { status: 400 });
+  }
   const generateSlug = (text) => {
     const randomString = Math.random().toString(36).substring(2, 8);
     return (
@@ -23,10 +26,21 @@ export async function POST(req) {
   const slug = generateSlug(title);
   const { jobData, jobError } = await supabase
     .from("jobs")
-    .insert([{ title, description, salary, admin_id, slug }])
+    .insert([
+      {
+        title: title,
+        description: description,
+        salary: salary,
+        admin_id: admin_id,
+        slug: slug,
+      },
+    ])
     .select("slug");
   if (jobError) {
     return new Response("Error", { status: 500 });
   }
-  return new Response(JSON.stringify({ message: "Success", slug: slug }), { status: 200, headers: { "Content-Type": "application/json" } });
+  return new Response(JSON.stringify({ message: "Success", slug: slug }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 }
